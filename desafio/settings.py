56a66da917ae246a6cf66a99d7675d9abf0dafe8
Matheus Santos <santos.matheus1012@gmail.com>
2024-10-13 10:18:45 -0300

    Atualizando a versão do python para 3.10 e do Django para 4.2.16

--- desafio/settings.py
+++ desafio/settings.py
@@ -121,13 +121,12 @@ print(BASE_DIR/'static')
 
 
 STATIC_URL = '/static/'
-
 if DEBUG:
     STATICFILES_DIRS = [
-        BASE_DIR/'..'/'static',
+        BASE_DIR/'static',
     ]
 else:
-    STATIC_ROOT = BASE_DIR/'..'/'static'
+    STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
